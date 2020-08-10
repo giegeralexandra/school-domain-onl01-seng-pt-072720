63ea46bd0895a85da6651a51b0725bdd6d88d092
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-10 01:14:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,10 +7,10 @@ class School
   def add_student(name, grade)
     @name = name 
     @grade = grade
-    if @roster.include?(grade) == false 
-      @roster = [] 
+    if @roster.include?(grade) == true 
       @roster[grade] << name 
-    else
+    else 
+      @roster = [] 
       @roster[grade] << name 
     end
   end
